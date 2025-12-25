be51a881f6acbd63b0d4ffa6306b13b157d4d992
Sanjana Kumari <sssanjanakumari789@gmail.com>
2025-12-26 01:03:57 +0530

    Day 1: First 5 Python programs

--- python_basics.py
+++ python_basics.py
@@ -1,1 +1,32 @@
+# 1. print Hello gsoc 2026 
 print("Hello GSoc 2026")
+
+#2.Take name input and print greeting
+
+name = input("enter your name")
+print("good morning" ,name)
+
+
+# 3.Add two numbers
+Num1 = 2
+Num2 = 4
+sum = Num1+Num2
+print(sum)
+
+
+
+# 4. Find area of a rectangle
+length = int(input("Enter the length"))
+breadth = int(input("Enter the breadth"))
+Area = (length * breadth)
+print(Area)
+
+
+
+# 5. Check even or odd
+num = int(input("Enter the value"))
+if num%2 == 0:
+     print("number is even ")
+else:
+     print("number is odd")    
+
